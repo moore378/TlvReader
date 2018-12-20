9b34ce4958e5850d04771c066c32ed0bae78a69e
Richard Moore <richard.h.moore.jr@gmail.com>
2018-12-19 19:24:38 -0800

    Initial dump

---
+++ TlvReader/TlvReader.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TlvReader
+{
+    partial class TlvReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHex = new System.Windows.Forms.RichTextBox();
+            this.dataGridViewTLV = new System.Windows.Forms.DataGridView();
+            this.btnMap = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTLV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtHex
+            // 
+            this.txtHex.Location = new System.Drawing.Point(12, 12);
+            this.txtHex.Name = "txtHex";
+            this.txtHex.Size = new System.Drawing.Size(706, 84);
+            this.txtHex.TabIndex = 0;
+            this.txtHex.Text = "";
+            // 
+            // dataGridViewTLV
+            // 
+            this.dataGridViewTLV.AllowUserToAddRows = false;
+            this.dataGridViewTLV.AllowUserToDeleteRows = false;
+            this.dataGridViewTLV.AllowUserToOrderColumns = true;
+            this.dataGridViewTLV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTLV.Location = new System.Drawing.Point(13, 137);
+            this.dataGridViewTLV.Name = "dataGridViewTLV";
+            this.dataGridViewTLV.ReadOnly = true;
+            this.dataGridViewTLV.Size = new System.Drawing.Size(705, 287);
+            this.dataGridViewTLV.TabIndex = 1;
+            // 
+            // btnMap
+            // 
+            this.btnMap.Location = new System.Drawing.Point(643, 104);
+            this.btnMap.Name = "btnMap";
+            this.btnMap.Size = new System.Drawing.Size(75, 23);
+            this.btnMap.TabIndex = 2;
+            this.btnMap.Text = "Map";
+            this.btnMap.UseVisualStyleBackColor = true;
+            this.btnMap.Click += new System.EventHandler(this.btnMap_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(562, 104);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load File";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(13, 102);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // TlvReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnMap);
+            this.Controls.Add(this.dataGridViewTLV);
+            this.Controls.Add(this.txtHex);
+            this.Name = "TlvReader";
+            this.Text = "TLV Reader";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTLV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtHex;
+        private System.Windows.Forms.DataGridView dataGridViewTLV;
+        private System.Windows.Forms.Button btnMap;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ TlvReader/TlvReader.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Ips.Tlv;
+using Ips.Tlv.Emv;
+using System.IO;
+//using System.Text.RegularExpressions;
+
+namespace TlvReader
+{
+    public partial class TlvReader : Form
+    {
+        //private bool valueHEX = false;
+
+        public TlvReader()
+        {
+            InitializeComponent();
+
+            //dataGridViewTLV.TopLeftHeaderCell.
+
+            //Setting up DataGridView
+            dataGridViewTLV.ColumnCount = 5;
+            dataGridViewTLV.Columns[0].Name = "TLV Tag Name";
+            dataGridViewTLV.Columns[0].Width = 125;
+            dataGridViewTLV.Columns[1].Name = "TLV Tag Code";
+            dataGridViewTLV.Columns[1].Width = 115;
+            dataGridViewTLV.Columns[2].Name = "Value (ASCII)";
+            dataGridViewTLV.Columns[2].Width = 150;
+            dataGridViewTLV.Columns[2].SortMode = DataGridViewColumnSortMode.NotSortable;
+            dataGridViewTLV.Columns[3].Name = "Value (HEX)";
+            dataGridViewTLV.Columns[3].Width = 150;
+            dataGridViewTLV.Columns[3].SortMode = DataGridViewColumnSortMode.NotSortable;
+            dataGridViewTLV.Columns[4].Name = "Type";
+            dataGridViewTLV.Columns[4].Width = 100;
+
+            dataGridViewTLV.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewTLV.MultiSelect = false;
+        }
+
+        private void btnMap_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string hex = this.txtHex.Text.Replace(",", "").Trim();
+                var standardSerializer = new TlvSerializer(EmvTagMap.FullDefault);
+                var tlvs = standardSerializer.Deserialize(hex);
+                populateGrid(tlvs, standardSerializer);
+                clearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ExceptionExtensions.ToDetailedString(ex), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK) // Test result.
+                {
+                    var filebytes = File.ReadAllBytes(openFileDialog1.FileName);
+                    var standardSerializer = new TlvSerializer(EmvTagMap.FullDefault);
+                    var tlvs = standardSerializer.Deserialize(filebytes);
+                    populateGrid(tlvs, standardSerializer);
+                    string hex = Ips.Tlv.ByteConverter.ByteArrayToHexString(filebytes);
+                    txtHex.Text = hex;
+
+                    clearForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ExceptionExtensions.ToDetailedString(ex), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void populateGrid(IEnumerable<ITlvNode> tlvs, TlvSerializer standardSerializer)
+        {
+            foreach (var item in tlvs)
+            {
+                var tag = standardSerializer.TagMap[item.Tag];
+                string strValue = item.Value.ToString();
+                string hexvalue = "";
+
+                if (tag.TlvType == TlvType.Binary)
+                {
+                    strValue = null;
+                    strValue = bytearraytostring((byte[])item.Value);
+                    hexvalue = Ips.Tlv.ByteConverter.ByteArrayToHexString((byte[])item.Value);
+                }
+                else
+                {
+                    foreach (char c in strValue)
+                    {
+                        hexvalue += String.Format("{0:X2}", Convert.ToInt32(c));
+                    }
+                }
+
+                dataGridViewTLV.Rows.Add(tag.Name, tag.Tag.ToString("X"), strValue, hexvalue, tag.TlvType);
+            }
+        }
+
+        private void clearForm()
+        {
+            txtHex.Text = null;
+            dataGridViewTLV.Rows.Clear();
+            dataGridViewTLV.Refresh();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ExceptionExtensions.ToDetailedString(ex), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void changeCellValue(int rownum, int colnum, string value)
+        {
+            dataGridViewTLV[colnum, rownum].Value = value;
+        }
+
+        private string bytearraytostring(byte[] arr)
+        {
+            string strValue = null;
+            foreach (var element in (byte[])arr) { strValue += element + " "; }
+            return strValue;
+        }
+    }
+}
